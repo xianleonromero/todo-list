1ce7799448f1404c16bb27e047941abac22f0400
xianleon310 <xleonr23@fpcoruna.afundacion.org>
2025-10-15 11:58:56 +0200

    useState para input

--- src/components/TaskInput.js
+++ src/components/TaskInput.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import './TaskInput.css';
 function TaskInput() {
-  return (
-    <div className="task-input-container">
-      <input type="text" placeholder="Nueva tarea..." />
-      <button>Añadir</button>
-    </div>
-  );
+    const[input,setInput]=useState('');
+    return (
+        <div className="task-input-container">
+            <input 
+                type="text" 
+                placeholder="Nueva tarea..." 
+                value={input} 
+                //cada vez que se escribe algo el texto que se acaba de escribir actualiza la variable input
+                onChange={(e)=>setInput(e.target.value)} 
+            />
+        <button>Añadir</button>
+        </div>
+    );
 }
 
 export default TaskInput;
