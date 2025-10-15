d6e6151607efb435a85d0a0f2716e119a70a8848
xianleon310 <xleonr23@fpcoruna.afundacion.org>
2025-10-15 12:31:52 +0200

    Version final de estilos de las tareas

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,12 @@
+import './TaskList.css';
 function TaskList() {
   const tareas = ['Comprar pan', 'Estudiar React'];
 
   return (
     //Recorre el array (cada elemento es "tarea" y el indice "index") y crea un li con el atributo key (que es el indice) y dentro el nombre de la tarea
-    <ul>
+    <ul className='task-list'>
         {tareas.map((tarea, index) => (
-            <li key={index}>{tarea}</li>
+            <li className="task-item" key={index}>{tarea}</li>
         ))}
     </ul>
   );
