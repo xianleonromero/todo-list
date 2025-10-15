59898fef21b7f9957d9b2429357221b8b42c0b73
xianleon310 <xleonr23@fpcoruna.afundacion.org>
2025-10-15 14:21:31 +0200

    movimiento de estado de tareas a App y uso de props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import TaskInput from './components/TaskInput';
 import TaskList from './components/TaskList';
 function App() {
+  const[tareas,setTareas]=useState(['Mock1','Mock2']);
   return (
     <div>
       <h1>Lista de tareas</h1>
       <TaskInput/>
-      <TaskList/>
+      <TaskList tareas={tareas}/>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,7 +1,5 @@
 import './TaskList.css';
-function TaskList() {
-  const tareas = ['Comprar pan', 'Estudiar React'];
-
+function TaskList({tareas}) {
   return (
     //Recorre el array (cada elemento es "tarea" y el indice "index") y crea un li con el atributo key (que es el indice) y dentro el nombre de la tarea
     <ul className='task-list'>
